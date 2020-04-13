191a47178269a07e78e03e8f5d5a91b2c1aea318
Simon Helmig <helmigsimon@gmail.com>
2020-04-13 12:07:43 +0200

    Migrated pipeline logic from .pkl files to a single .py file due to pickling issues across environments

---
+++ lib/pipelines.py
@@ -0,0 +1,42 @@
+from sklearn.pipeline import Pipeline
+from .transformers import *
+
+#Extracted Data Pipe
+extracted_pipe = Pipeline([
+    ('remove_id', ColumnRemover('id')),
+    ('unpickle', Unpickler(['track_titles'])),
+    ('make_market_value', ColumnCombiner('median','market_price','market_value')),
+    ('remove_duplicates', DuplicateRemover('release_id')),
+    ('remove_nulls',NullRemover('market_value')),
+    ('count_standards',StandardCountEncoder('track_titles')),
+    ('count_days_since_last_sale',LastSoldEncoder(feature='last_sold',new_feature='days_since_last_sale'))
+])
+
+#API Pipes
+clean_text_pipe = Pipeline([
+    ('label', LabelCleaner()),
+    ('artist', ArtistCleaner())
+])
+
+column_encoding_pipe = Pipeline([
+    ('country',CountryEncoder()),
+    ('genre',GenreEncoder()),
+    ('style', MultiValueCategoricalEncoder(feature='style'))
+])
+
+format_pipe = Pipeline([
+    ('make_columns', FormatEncoder()),
+    ('encode_descriptions',MultiValueCategoricalEncoder('format_description')),
+    ('clean_format_text',FormatTextCleaner())
+])
+
+api_pipe = Pipeline([
+    ('remove_columns', ColumnRemover('id')),
+    ('split_title', TitleSplitter()),
+    ('unpickle', Unpickler(['genre','style','label','formats'])),
+    ('clean_text',clean_text_pipe),
+    ('remove_duplicates', DuplicateRemover('release_id')),
+    ('encode_columns',column_encoding_pipe),
+    ('format_columns', format_pipe),
+    ('encode_time_periods', TimePeriodEncoder())
+])
