a18bd5cf7f86c62057c0f9d3ab09c7c767debac6
Simon Helmig <helmigsimon@gmail.com>
2020-04-14 19:21:34 +0200

    Updated data/util/paths.py to use the correct drive folder and copied colab changes to local repository

--- data/util/paths.py
+++ data/util/paths.py
@@ -5,7 +5,7 @@ FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 ROOT_PATH = Path(FILE_PATH).parent
 try:
     from google.colab import drive
-    DATA_PATH = DRIVE_PATH = os.path.join(os.path.dirname(os.getcwd()),'drive/My Drive/EPFL Capstone Project')
+    DATA_PATH = DRIVE_PATH = os.path.join(os.path.dirname(os.getcwd()),'drive/My Drive/EPFL Capstone')
 except ModuleNotFoundError:
     DATA_PATH = os.path.join(ROOT_PATH,'data')
 API_PATH = os.path.join(ROOT_PATH,'api')
